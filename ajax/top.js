c0be979dfe350466526de06fb957be3c923c2bf7
Sudarshan Bangalore <bsudarshan@outlook.com>
2018-12-08 04:42:39 -0500

    forgot files!

---
+++ ajax/top.js
@@ -0,0 +1,7 @@
+var tophtml = `
+       <h4 onclick="location.href='index.html';"><u>Sudarshan (~Sean) Bangalore</u></h4>
+	<button onclick="location.href='resume.html';" class="button primary">Resume</button>
+	&nbsp &nbsp
+	<button onclick="location.href='notes.html';" class="button primary">Notes</button>
+`;
+$(".top").append(tophtml);
