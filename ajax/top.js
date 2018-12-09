c9be01a3960bdd77d88f47030f1e245d85e87fd8
Sudarshan Bangalore <bsudarshan@outlook.com>
2018-12-09 12:36:40 -0500

    added resume button and removed underline from head

--- ajax/top.js
+++ ajax/top.js
@@ -1,5 +1,7 @@
 var tophtml = `
-       <h4 onclick="location.href='index.html';"><u>Sudarshan (~Sean) Bangalore</u></h4>
+       <h4 onclick="location.href='index.html';">Sudarshan (~Sean) Bangalore</h4>
+	<button onclick="location.href='index.html';" class="button primary">Home</button>
+	&nbsp &nbsp
 	<button onclick="location.href='resume.html';" class="button primary">Resume</button>
 	&nbsp &nbsp
 	<button onclick="location.href='notes.html';" class="button primary">Notes</button>
